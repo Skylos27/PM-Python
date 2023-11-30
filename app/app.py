9b176c0c05e74f0beef69dafa5b498c4dba667a4
Alexis Lefrancois <alexis.lefrancois@epita.fr>
2023-11-30 22:58:44 +0100

    Mise à jour des commentaires

--- app/app.py
+++ app/app.py
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from pm import PasswordManager
-from flask import jsonify
 
 
 app = Flask(__name__)
 
 password_manager = PasswordManager()
 
-# Utilisez une liste pour stocker temporairement les informations de connexion (à des fins de démonstration)
+# Liste pour stocker temporairement les informations de connexion
 users = [{'username': 'admin', 'password': 'admin', 'entries': []}]
-
-# Utilisez une autre liste pour stocker les données du fichier entries.txt
+# Liste pour stocker les données du fichier entries.txt
 db = []
 def update_db():
     with open('entries.txt', 'r') as file:
@@ -23,8 +21,16 @@ def update_db():
 update_db()
 
 
+# Définition des différentes routes de l'application
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    """
+    Route pour la page d'accueil
+    
+    :return: page d'accueil
+    :rtype: template
+    """
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -38,11 +44,26 @@ def home():
 
 @app.route('/edit/<site>', methods=['GET', 'POST'])
 def edit_entry(site):
-
+    """
+    Route pour la page d'édition d'une entrée
+    
+    :param site: site de l'entrée à éditer
+    :type site: str
+    :return: page d'édition d'une entrée
+    :rtype: template
+    """
     return redirect(url_for('dashboard', username='admin'))
 
 @app.route('/delete_entry/<string:site>')
 def delete_entry(site):
+    """
+    Route pour la suppression d'une entrée
+    
+    :param site: site de l'entrée à supprimer
+    :type site: str
+    :return: page de dashboard
+    :rtype: template
+    """
     password_manager.delete_pass(site)
     index_to_remove = None
     for i, entry in enumerate(db):
@@ -60,6 +81,14 @@ def delete_entry(site):
 
 @app.route('/dashboard/<username>')
 def dashboard(username):
+    """
+    Route pour la page de dashboard
+    
+    :param username: nom d'utilisateur
+    :type username: str
+    :return: page de dashboard
+    :rtype: template
+    """
     update_db()
     user = get_user_by_username(username)
     if user:
@@ -70,6 +99,12 @@ def dashboard(username):
 
 @app.route('/add_entry', methods=['GET', 'POST'])
 def add_entry():
+    """
+    Route pour la page d'ajout d'une entrée
+    
+    :return: page d'ajout d'une entrée
+    :rtype: template
+    """
     if request.method == 'POST':
         site = request.form['site']
         username = request.form['username']
@@ -80,14 +115,33 @@ def add_entry():
         return redirect(url_for('dashboard', username='admin'))
     return render_template('add_entry.html')
 
-
+# Getter pour récupérer un utilisateur par son username et son mot de passe
 def get_user(username, password):
+    """
+    Récupère un utilisateur par son nom d'utilisateur et son mot de passe
+    
+    :param username: nom d'utilisateur
+    :type username: str
+    :param password: mot de passe
+    :type password: str
+    :return: utilisateur
+    :rtype: dict
+    """
     for user in users:
         if user['username'] == username and user['password'] == password:
             return user
     return None
 
+# Getter pour récupérer un utilisateur par son username et son mot de passe
 def get_user_by_username(username):
+    """
+    Récupère un utilisateur par son nom d'utilisateur
+    
+    :param username: nom d'utilisateur
+    :type username: str
+    :return: utilisateur
+    :rtype: dict
+    """
     for user in users:
         if user['username'] == username:
             return user
--- app/pm.py
+++ app/pm.py
@@ -9,6 +9,14 @@ class PasswordManager:
         self.login_dict = {}
     
     def check_site(self, site1):
+        """
+        Vérifie si le site existe déjà dans le fichier
+
+        :param site1: site à vérifier
+        :type site1: str
+        :return: True si le site existe déjà, False sinon
+        :rtype: bool
+        """
         existing = False
         with open("./entries.txt",'r') as f:
             for line in f:
@@ -18,31 +26,75 @@ class PasswordManager:
         return existing
                 
     def create_key(self,path):
+        """
+        Crée une clé et l'écrit dans un fichier
+        
+        :param path: chemin du fichier où écrire la clé
+        :type path: str
+        :return: None
+        """
         self.key = Fernet.generate_key()
         with open(path,'wb') as f:
             f.write(self.key)
         print(self.key)
 
     def load_key(self, path):
+        """
+        Charge une clé depuis un fichier
+
+        :param path: chemin du fichier où lire la clé
+        :type path: str
+        :return: None
+        """
         with open(path,'rb') as f:
             self.key = f.read()
 
     def decrypt_password(self, encrypted_password):
+        """
+        Déchiffre un mot de passe
+
+        :param encrypted_password: mot de passe chiffré
+        :type encrypted_password: str
+        :return: mot de passe déchiffré
+        :rtype: str
+        """
         decrypted_password = Fernet(self.key).decrypt(encrypted_password.encode()).decode()
         return decrypted_password
 
     def encrypt_password(self, encrypted_password):
+        """
+        Chiffre un mot de passe
+        
+        :param encrypted_password: mot de passe à chiffrer
+        :type encrypted_password: str
+        :return: mot de passe chiffré
+        :rtype: str
+        """
         encrypted = Fernet(self.key).encrypt(encrypted_password.encode())
         return encrypted
 
 
     def list_sites(self, path = "./entries.txt"):
+        """
+        Affiche la liste des sites enregistrés
+
+        :param path: chemin du fichier où lire les sites
+        :type path: str
+        :return: None
+        """
         with open(path,'r') as f:
             for line in f:
                 site,login, encrypted = line.split(":")
                 print("- " + site + "\n")
 
     def load_pass(self,path):
+        """
+        Charge les mots de passe depuis un fichier
+
+        :param path: chemin du fichier où lire les mots de passe
+        :type path: str
+        :return: None
+        """
         self.password_file = path
         with open(path,'r') as f:
             for line in f:
@@ -51,6 +103,17 @@ class PasswordManager:
                 self.login_dict[site] = login
 
     def add_password(self, site, login, password):
+        """
+        Ajoute un mot de passe à la liste
+        
+        :param site: site du mot de passe
+        :type site: str
+        :param login: login du site
+        :type login: str
+        :param password: mot de passe du site
+        :type password: str
+        :return: None
+        """
         self.password_dict[site] = password
         self.login_dict[site] = login
         if self.check_site(site):
@@ -62,25 +125,47 @@ class PasswordManager:
                 f.write(site + ":" + login + ":" + encrypted.decode() + "\n")
 
     def get_pass(self,site):
+        """
+        Retourne le mot de passe d'un site
+        
+        :param site: site dont on veut le mot de passe
+        :type site: str
+        :return: mot de passe du site
+        :rtype: str
+        """
         if self.check_site(site) == False:
             print("Ce site n'existe pas")
             return
         return self.password_dict[site]
     
     def get_login(self,site):
+        """
+        Retourne le login d'un site
+
+        :param site: site dont on veut le login
+        :type site: str
+        :return: login du site
+        :rtype: str
+        """
         if self.check_site(site) == False:
             print("Ce site n'existe pas")
             return
         return self.login_dict[site]
     
     def delete_pass(self,site):
-        # Open the file in read mode to read its contents
+        """
+        Supprime un mot de passe
+
+        :param site: site dont on veut supprimer le mot de passe
+        :type site: str
+        :return: None
+        """
+        # Ouvre le fichier en mode lecture seule et récupère toutes les lignes
         with open('./entries.txt', 'r') as file:
             lines = file.readlines()
-
-        # Open the file in write mode to overwrite its contents
+        # Ouvre le fichier en mode écriture et supprime le contenu
         with open('./entries.txt', 'w') as file:
-            # Write all lines except those containing the specified word
+            # Parcours toutes les lignes du fichier et supprime la ligne si le site est présent
             for line in lines:
                 if site not in line:
                     file.write(line)
